374259ddd808ca24ef8a4ed37a0a535897bcf683
CulturedNiichan <oniichanhaou@yahoo.com>
2023-07-07 12:57:33 +0200

    Initial commi

---
+++ QuoteReplacer.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace remove_double_quotes
+{
+    public class QuoteReplacer
+    {
+        public string ReplaceDuplicateQuotes(string input)
+        {
+            string pattern = "\"{2,}";
+            string replacement = "\"";
+            string result = Regex.Replace(input, pattern, replacement);
+
+            return result;
+        }
+    }
+}
